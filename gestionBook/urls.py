6915b64482cbaaa11d709ef55378348e2ba66cb8
Laurent Sun <lapoudrerouge@gmail.com>
2025-04-15 17:36:43 +0200

    Added new URL's for the get_info function in view.

--- gestionBook/urls.py
+++ gestionBook/urls.py
@@ -3,5 +3,6 @@ from django.urls import  path
 from . import  views
 
 urlpatterns = [
-    path('interface', views.add_book_webpage,name='add_book_w'),
+    path('add', views.book_add_webpage, name='book.add_w'),
+    path('get_info', views.book_get_info_webpage, name='book.get_info_w'),
 ]
--- gestionBook/views.py
+++ gestionBook/views.py
@@ -1,35 +1,52 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from . import input_book as imbk
+from .book_management import book
+
+
 # Create your views here.
 
-# send ======================================
-def add_book_webpage(request):
+def book_add_webpage(request):
     if request.method == 'POST':
 
         book_id = request.POST.get('bookID')
-        authors_raw = request.POST.get('author')
-        tags_raw = request.POST.get('tags')
+        authors_raw = request.POST.get('author').lower()
+        tags_raw = request.POST.get('tags').lower()
 
         # Split authors and tags
         authors_list = [author.strip() for author in authors_raw.split(' ')]
         tags_list = [tag.strip() for tag in tags_raw.split(' ')]
 
         # register it in the database
-        result = imbk.input_book(book_id, authors_list, tags_list)
+        result = book.add(book_id, authors_list, tags_list)
 
         # check if success or already present
-        if result["book"]=='already exist':
-            return HttpResponse(f"Book with ID {book_id} already exist.")
-        else:
+        if result["created"]:
             context = {
                 "book_id": book_id,
                 "authors": authors_list,
                 "tags": tags_list
             }
-            return render(request, 'add_book/book_added.html',context)
+            return render(request, 'book/add/book_added.html', context)
+        else:
+            return HttpResponse(f"Book with ID {book_id} already exist.")
 
     # If the method is not POST, render the form (GET request)
-    return render(request, 'add_book/webpage.html')
+    return render(request, 'book/add/webpage.html')
 
+
+def book_get_info_webpage(request):
+    if request.method == 'POST':
+        book_id = request.POST.get('bookID')
+
+        result = book.get_info(book_id)
+        if result['success']:
+            context = {
+                "book_id": book_id,
+                "authors": result['authors'],
+                "tags": result['tags']
+            }
+            return  HttpResponse(f"Error: {result['authors']}")#render(request,'book/get_info/info_book.html')
+        else:
+            return HttpResponse(f"Error: {result['error']}")
+    return render(request, 'book/get_info/ask.html')
