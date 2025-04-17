8dad86badbeb5c5493e94a712a19b427def598e6
Laurent Sun <lapoudrerouge@gmail.com>
2025-04-17 17:20:39 +0200

    commit to add authentication app.

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+import json
+from authlib.integrations.django_client import OAuth
+from django.conf import settings
+from django.shortcuts import redirect, render, redirect
+from django.urls import reverse
+from urllib.parse import quote_plus, urlencode
+from django.contrib.auth.models import User
+import django.contrib.auth as auth
 
-# Create your views here.
+oauth = OAuth()
+
+oauth.register(
+    "auth0",
+    client_id=settings.AUTH0_CLIENT_ID,
+    client_secret=settings.AUTH0_CLIENT_SECRET,
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f"https://{settings.AUTH0_DOMAIN}/.well-known/openid-configuration",
+)
+
+
+def index(request):
+
+    return render(
+        request,
+        "index.html",
+        context={
+            "connected": request.user.is_authenticated,
+            "session": request.user.username,
+            "pretty":request.user.id,
+        },
+    )
+
+
+def callback(request):
+    token = oauth.auth0.authorize_access_token(request)
+    user_info = token['userinfo']
+
+    email = user_info['email']
+    username = user_info.get('nickname', email.split('@')[0])
+
+    user, _ = User.objects.get_or_create(username=username, defaults={'email': email})
+    if user is not None:
+        auth.login(request, user)
+
+    return redirect(request.build_absolute_uri(reverse("index")))
+
+
+def login(request):
+    return oauth.auth0.authorize_redirect(
+        request, request.build_absolute_uri(reverse("callback"))
+    )
+
+
+def logout(request):
+    request.session.clear()
+
+    return redirect(
+        f"https://{settings.AUTH0_DOMAIN}/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": request.build_absolute_uri(reverse("index")),
+                "client_id": settings.AUTH0_CLIENT_ID,
+            },
+            quote_via=quote_plus,
+        ),
+    )
